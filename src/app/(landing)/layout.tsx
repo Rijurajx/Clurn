82b1715490ef5e3e642908e65e2dc152a4457065
Rijurajx <iphonerijuraj@gmail.com>
2024-09-20 02:40:41 +0530

    feat: added landing page

---
+++ src/app/(landing)/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import LandingPageNavbar from "./_components/navbar"
+const LandingPageLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className="flex flex-col container relative">
+            <LandingPageNavbar></LandingPageNavbar>
+            {children}
+        </div>
+    )
+}
+
+export default LandingPageLayout
